623d4437f141a5df7e407031c16b7390d3879c34
UgurCkmkc <1335002@dogus.edu.tr>
2017-03-25 23:48:52 +0300

    Login Ekranı Ekleniyor

--- app/src/main/java/com/meuf/sosyalkulup/SignUp.java
+++ app/src/main/java/com/meuf/sosyalkulup/SignUp.java
@@ -2,7 +2,6 @@ package com.meuf.sosyalkulup;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -28,8 +27,6 @@ public class SignUp extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        Intent intent = getIntent();
-
         btn_sign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,17 +44,19 @@ public class SignUp extends Activity {
     private  void register(String username,String password,String email){
         String urlsuffix = "?username=" + username +"&password=" + password + "&email=" + email;
         class RegisterUser extends AsyncTask<String,Void,String>{
+
             ProgressDialog loading;
 
             @Override
             protected void onPreExecute(){
                 super.onPreExecute();
-                loading = ProgressDialog.show(SignUp.this,"Lütfen Bekleyiniz",null, true,true);
+                loading = ProgressDialog.show(SignUp.this,"Lütfen Bekleyiniz",null,true,true);
             }
             @Override
             protected void onPostExecute(String s){
                 super.onPostExecute(s);
-                Toast.makeText(getApplicationContext(),"Internet bağlantınızı kontrol ediniz.",Toast.LENGTH_SHORT).show();
+                loading.dismiss();
+                Toast.makeText(getApplicationContext(),"Kayıt Başarılı.",Toast.LENGTH_SHORT).show();
             }
 
             @Override
