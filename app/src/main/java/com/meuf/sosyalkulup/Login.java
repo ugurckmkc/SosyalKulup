b092ddb5f6a8c3a083b73f8b3fff04bb2c152344
UgurCkmkc <1335002@dogus.edu.tr>
2017-03-26 19:39:47 +0300

    Login Ekranı Eklendi (Tasarımı Yapılacak) -- Start Ekranı da eklendi -- Boş bırakılırsa Hata Mesajları eklendi

--- app/src/main/java/com/meuf/sosyalkulup/Login.java
+++ app/src/main/java/com/meuf/sosyalkulup/Login.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -38,6 +40,8 @@ public class Login extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
 
         editTextEmail=(EditText)findViewById(R.id.editText_email);
