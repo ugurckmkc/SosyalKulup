a948a03b85d5d8e5504a4286a9d39ecf5096b68a
UgurCkmkc <1335002@dogus.edu.tr>
2017-12-01 00:08:40 +0300

    Proje Baştan Yazıldı

--- app/src/main/java/com/meuf/sosyalkulup/Fragments/KulupDuyuruFragment.java
+++ app/src/main/java/com/meuf/sosyalkulup/Fragments/KulupDuyuruFragment.java
@@ -1,6 +1,8 @@
 package com.meuf.sosyalkulup.Fragments;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -35,18 +37,23 @@ public class KulupDuyuruFragment extends Fragment {
         Title = (TextView) view.findViewById(R.id.text_title);
         Message = (TextView) view.findViewById(R.id.text_message);
 
-        Bundle bundle = getActivity().getIntent().getExtras();
 
-        if(bundle !=null){
-            for(String key : bundle.keySet()){
-                if(key.equals("title"))
-                    Title.setText(bundle.getString(key));
-                else if(key.equals("message"))
-                    Message.setText(bundle.getString(key));
-            }
-        }
 
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        SharedPreferences sharedPref = getActivity().getSharedPreferences("com.meuf.sosyalkulup", Context.MODE_PRIVATE);
+
+        String Title1 = sharedPref.getString("title", "Etkinlik Bulunamadı");
+        String Message1 = sharedPref.getString("message", "Etkinlik açıklaması bulunamadı!");
+
+        Title.setText(Title1);
+        Message.setText(Message1);
+
+
     }
 
     public interface OnFragmentInteractionListener {
--- app/src/main/java/com/meuf/sosyalkulup/authentication/StartScreen.java
+++ app/src/main/java/com/meuf/sosyalkulup/authentication/StartScreen.java
@@ -1,7 +1,9 @@
 package com.meuf.sosyalkulup.authentication;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
@@ -11,6 +13,8 @@ import com.meuf.sosyalkulup.R;
 
 public class StartScreen extends Activity {
 
+    private String Title,Message;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +22,9 @@ public class StartScreen extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_start_screen);
 
-            Thread th = new Thread() {
+
+
+        Thread th = new Thread() {
                 @Override
                 public void run() {
                     try {
@@ -26,6 +32,21 @@ public class StartScreen extends Activity {
                     } catch (Exception e) {
                         e.printStackTrace();
                     } finally {
+                        SharedPreferences sharedPref = getSharedPreferences("com.meuf.sosyalkulup", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPref.edit();
+
+                        if(getIntent().getExtras() !=null){
+                            for(String key : getIntent().getExtras().keySet()){
+                                if(key.equals("title"))
+                                    Title = getIntent().getExtras().getString(key);
+                                else if(key.equals("message"))
+                                    Message = getIntent().getExtras().getString(key);
+                            }
+                        }
+                        editor.putString("title", Title);
+                        editor.putString("message", Message);
+                        editor.commit();
+
                         Intent obj = new Intent(StartScreen.this, Login.class);
                         startActivity(obj);
                     }
@@ -35,10 +56,28 @@ public class StartScreen extends Activity {
             th.start();
         }
 
-        @Override
-        protected void onPause(){
-            super.onPause();
-            finish();
-        }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+       // sharedPref.edit().putString("title",Title).apply();
+       // sharedPref.edit().putString("message", Message).apply();
+
+       // KulupDuyuruFragment KulupDuyuru = new KulupDuyuruFragment();
+
+        /*Bundle bundle = new Bundle();
+        bundle.putString("title",Title);
+        bundle.putString("message",Message);
+        KulupDuyuru.setArguments(bundle);*/
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
 }
 
